1bb8bbe1804108a7c8333396554ccd8da022b4a7
Zakhar Voloshchuk <3elduu@gmail.com>
2024-02-23 09:47:19 +0200

    update nixpkgs to unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "Petafloppa's system configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
