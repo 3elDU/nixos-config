5a2d683e355a03cb5b3f540898cc962daa290457
Zakhar Voloshchuk <3elduu@gmail.com>
2024-02-23 09:48:06 +0200

    permit installing electron for obsidian

--- home/desktop/text.nix
+++ home/desktop/text.nix
@@ -1,4 +1,7 @@
 { pkgs, ... }: {
+  # Obsidian requires EOL version of electron
+  nixpkgs.config.permittedInsecurePackages = [ "electron-25.9.0" ];
+
   home.packages = with pkgs; [
     libreoffice
     obsidian
