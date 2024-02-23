f10b96a7be281c9d051f4c73ebd20da52cda8462
Zakhar Voloshchuk <3elduu@gmail.com>
2024-02-23 09:32:44 +0200

    Add mako

---
+++ home/wm/mako.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }: {
+  services.mako = {
+    enable = true;
+
+    font = "JetBrainsMono Nerd Font Propo 12";
+
+    margin = 8;
+    padding = 8;
+
+    backgroundColor = "#282828BF";
+    textColor = "#FBF1C7FF";
+    progressColor = "D79921FF";
+    borderColor = "#D79921FF";
+
+    borderRadius = 8;
+    borderSize = 2;
+  };
+}
