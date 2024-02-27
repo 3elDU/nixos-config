01438e452ffa6a3880c10ea80a2feb1b677c5670
Zakhar Voloshchuk <3elduu@gmail.com>
2024-02-27 13:51:22 +0200

    update foot config

--- home/desktop/foot.nix
+++ home/desktop/foot.nix
@@ -5,9 +5,11 @@
       main = {
         shell = "nu";
         font = "JetBrainsMono Nerd Font:size=10";
+        pad = "8x4";
       }; 
       colors = {
         alpha = 0.85;
+        scrollback-indicator = "000000 D79921";
       };
     };
   };
