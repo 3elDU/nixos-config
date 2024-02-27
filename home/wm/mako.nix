ecbea698545aa81d5a50ea91a854591b1434e298
Zakhar Voloshchuk <3elduu@gmail.com>
2024-02-27 18:33:44 +0200

    mako: tweak config - lower font size - anchor the notifications at top-center - remove background transparency - fix progressColor property (forgot a pound sign)

--- home/wm/mako.nix
+++ home/wm/mako.nix
@@ -2,14 +2,15 @@
   services.mako = {
     enable = true;
 
-    font = "JetBrainsMono Nerd Font Propo 12";
+    font = "JetBrainsMono Nerd Font Propo 10";
 
-    margin = "8";
+    anchor = "top-center";
+    margin = "6";
     padding = "8";
 
-    backgroundColor = "#282828BF";
+    backgroundColor = "#282828FF";
     textColor = "#FBF1C7FF";
-    progressColor = "D79921FF";
+    progressColor = "#D79921FF";
     borderColor = "#D79921FF";
 
     borderRadius = 8;
