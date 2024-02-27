e70d1be842571cd4ba4ed28a1f707560083196f3
Zakhar Voloshchuk <3elduu@gmail.com>
2024-02-27 13:50:41 +0200

    update nushell config

--- home/shell.nix
+++ home/shell.nix
@@ -2,7 +2,36 @@
   programs.nushell = {
     enable = true;
     package = pkgs.nushellFull;
+
+    configFile.source = ../configs/nushell/config.nu;
+    envFile.source = ../configs/nushell/env.nu;
+
+    environmentVariables = {
+      EDITOR = "nvim";
+      MANPAGER = "nvim +Man!";
+    };
+
+    shellAliases = {
+      ll = "^ls -lAh";
+
+      gcn = "git clone";
+      gs = "git status";
+      ga = "git add";
+      gd = "git diff";
+      gc = "git commit";
+      gp = "git push";
+
+      n = "nvim";
+      h = "htop";
+      d = "^du -h1 -d1 -c .";
+
+      nr = "sudo nixos-rebuild switch";
+    };
   };
+
+  # This file is sourced from config.nu
+  xdg.configFile."nushell/gruvbox-dark-hard.nu".source = ../configs/nushell/gruvbox-dark-hard.nu;
+
   # External command completions used in nushell
   programs.carapace = {
     enable = true;
