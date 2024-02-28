8882413087c4c366f7a00f9eda67df8ed6a8acb3
Zakhar Voloshchuk <3elduu@gmail.com>
2024-02-28 19:01:38 +0200

    feat(home/xdg): enable gtk and wlr portals by default

--- home/xdg.nix
+++ home/xdg.nix
@@ -1,16 +1,29 @@
 { config, pkgs, ... }: {
-  xdg.enable = true;
-  xdg.userDirs = {
+  xdg = {
     enable = true;
-    createDirectories = true;
 
-    download = "${config.home.homeDirectory}/dl";
-    documents = "${config.home.homeDirectory}/doc";
-    desktop = "${config.home.homeDirectory}/desk";
-    music = "${config.home.homeDirectory}/mus";
-    pictures = "${config.home.homeDirectory}/pic";
-    videos = "${config.home.homeDirectory}/vid";
-    templates = "${config.home.homeDirectory}/tmpl";
-    publicShare = "${config.home.homeDirectory}/pub";
+    portal = {
+      enable = true;
+      config.common = {
+        default = [ "wlr" "gtk" ];
+        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+      };
+      extraPortals = [ pkgs.xdg-desktop-portal-wlr pkgs.xdg-desktop-portal-gtk ];
+      xdgOpenUsePortal = true;
+    };
+
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+
+      download = "${config.home.homeDirectory}/dl";
+      documents = "${config.home.homeDirectory}/doc";
+      desktop = "${config.home.homeDirectory}/desk";
+      music = "${config.home.homeDirectory}/mus";
+      pictures = "${config.home.homeDirectory}/pic";
+      videos = "${config.home.homeDirectory}/vid";
+      templates = "${config.home.homeDirectory}/tmpl";
+      publicShare = "${config.home.homeDirectory}/pub";
+    };
   };
 }
