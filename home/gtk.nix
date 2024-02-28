11e5b1f2e699cec75574df5f7fe96b9ab7b7c0c0
Zakhar Voloshchuk <3elduu@gmail.com>
2024-02-28 15:15:32 +0200

    feat(gtk): remove cursor theming, as it is already specified in desktop/cursor.nix

--- home/gtk.nix
+++ home/gtk.nix
@@ -1,10 +1,6 @@
 { pkgs, ... }: {
   gtk = {
     enable = true;
-    cursorTheme = {
-      package = pkgs.simp1e-cursors;
-      name = "Simp1e-Gruvbox-Dark";
-    };
     theme = {
       package = pkgs.adw-gtk3;
       name = "adw-gtk3";
