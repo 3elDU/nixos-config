e29ffc3ee5a613ef28a35060ae61f14cbd9b3e68
Zakhar Voloshchuk <3elduu@gmail.com>
2024-03-02 01:39:13 +0200

    feat(neovim): add mappings for barbar and gitsigns - display git status in tab title (modified, added, deleted)

--- configs/nvim/lua/plugins/barbar.lua
+++ configs/nvim/lua/plugins/barbar.lua
@@ -12,6 +12,43 @@ return {
     auto_hide = true,
     icons = {
       button = '',
-    }
+      gitsigns = {
+        added = { enabled = true, icon = '+' },
+        changed = { enabled = true, icon = '~' },
+        deleted = { enabled = true, icon = '-' },
+      }
+    },
   },
+  config = function ()
+    local map = vim.keymap.set
+
+    -- Previous / Next tab
+    map('n', '<leader>tt', '<Cmd>BufferNext<CR>')
+    map('n', '<leader>tT', '<Cmd>BufferPrevious<CR>')
+    -- Reorder current rab
+    map('n', '<leader>trt', '<Cmd>BufferMoveNext<CR>')
+    map('n', '<leader>trT', '<Cmd>BufferMovePrevious<CR>')
+    -- Go to a specific tab
+    map('n', '<leader>t1', '<Cmd>BufferGoto 1<CR>')
+    map('n', '<leader>t2', '<Cmd>BufferGoto 2<CR>')
+    map('n', '<leader>t3', '<Cmd>BufferGoto 3<CR>')
+    map('n', '<leader>t4', '<Cmd>BufferGoto 4<CR>')
+    map('n', '<leader>t5', '<Cmd>BufferGoto 5<CR>')
+    map('n', '<leader>t6', '<Cmd>BufferGoto 6<CR>')
+    map('n', '<leader>t7', '<Cmd>BufferGoto 7<CR>')
+    map('n', '<leader>t8', '<Cmd>BufferGoto 8<CR>')
+    map('n', '<leader>t9', '<Cmd>BufferGoto 9<CR>')
+    map('n', '<leader>t0', '<Cmd>BufferLast<CR>')
+    -- Pin/unpin buffer
+    map('n', '<leader>tp', '<Cmd>BufferPin<CR>')
+    -- Close buffer
+    map('n', '<leader>c', '<Cmd>BufferClose<CR>')
+    -- Enter buffer-picking mode
+    map('n', '<leader>tp', '<Cmd>BufferPick<CR>')
+    -- Sort tabs by
+    map('n', '<leader>tsn', '<Cmd>BufferOrderByBufferNumber<CR>')
+    map('n', '<leader>tsd', '<Cmd>BufferOrderByDirectory<CR>')
+    map('n', '<leader>tsl', '<Cmd>BufferOrderByLanguage<CR>')
+    map('n', '<leader>tsw', '<Cmd>BufferOrderByWindowNumber<CR>')
+  end
 }
--- configs/nvim/lua/plugins/gitsigns.lua
+++ configs/nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,35 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = true,
+  opts = {
+    current_line_blame = true,
+    current_line_blame_opts = {
+      delay = 250,
+    },
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns;
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Stage/unstage current line
+      map('n', '<leader>gs', gs.stage_hunk)
+      map('n', '<leader>gr', gs.reset_hunk)
+      -- Same, but in visual mode
+      map('v', '<leader>gs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+      map('v', '<leader>gr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+      map('n', '<leader>gu', gs.undo_stage_hunk)
+      -- Stage/reset the whole buffer
+      map('n', '<leader>gS', gs.stage_buffer)
+      map('n', '<leader>gR', gs.reset_buffer)
+      -- Diff, blame, pewview, deleted
+      map('n', '<leader>gd', gs.diffthis)
+      map('n', '<leader>gp', gs.preview_hunk)
+      map('n', '<leader>gb', function() gs.blame_line { full = true } end)
+      map('n', '<leader>gd', gs.toggle_deleted)
+    end
+  },
 }
