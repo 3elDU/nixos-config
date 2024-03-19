ca5f157ff77f4378738db1665de851f1747e7feb
Zakhar Voloshchuk <3elduu@gmail.com>
2024-03-19 18:11:25 +0200

    feat(tmux): tweak config

--- home/tmux.nix
+++ home/tmux.nix
@@ -6,18 +6,32 @@
     mouse = true;
     prefix = "C-Space";
     shell = "${pkgs.nushell}/bin/nu";
+    plugins = with pkgs; [
+      {
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          # Don't fill unactive windows at all
+          set -g @catppuccin_window_default_fill "none"
+          # Fill the selected windows
+          set -g @catppuccin_window_current_fill "all"
+          set -g @catppuccin_window_middle_separator " "
+
+          set -g @catppuccin_status_modules_right "application session date_time"
+          set -g @catppuccin_status_left_separator  ""
+          set -g @catppuccin_status_fill "all"
+        '';
+      }
+    ];
     extraConfig = ''
       set-option -u default-terminal
       # Enable truecolor support
       set-option -ga terminal-overrides ",xterm*:Tc"
 
       # Reduce escape time
-      set-option -g escape-time 20
+      set-option -g escape-time 0
+
+      # Switch to next window on double C-Space
+      bind C-Space next-window
     '';
-    plugins = with pkgs; [
-      {
-        plugin = tmuxPlugins.catppuccin;
-      }
-    ];
   };
 }
