0519d5646dbf047c50ed8d2bbf72a31abd065970
Zakhar Voloshchuk <3elduu@gmail.com>
2024-03-19 14:03:18 +0200

    feat(tmux): configure tmux - enable truecolor support - reduce escape time to 20ms - add catppuccin plugin

--- home/tmux.nix
+++ home/tmux.nix
@@ -6,5 +6,18 @@
     mouse = true;
     prefix = "C-Space";
     shell = "${pkgs.nushell}/bin/nu";
+    extraConfig = ''
+      set-option -u default-terminal
+      # Enable truecolor support
+      set-option -ga terminal-overrides ",xterm*:Tc"
+
+      # Reduce escape time
+      set-option -g escape-time 20
+    '';
+    plugins = with pkgs; [
+      {
+        plugin = tmuxPlugins.catppuccin;
+      }
+    ];
   };
 }
