fd440077806d1c24d68b05e79813018c40b4fc9c
Zakhar Voloshchuk <3elduu@gmail.com>
2024-06-14 15:03:03 +0300

    feat(avizo): remove border radius

--- home/wm/avizo.nix
+++ home/wm/avizo.nix
@@ -9,15 +9,15 @@
         height = 172;
         padding = 12;
 
-        border-radius = 12;
-        border-width = 4;
+        border-width = 2;
+        border-radius = 0;
 
         block-height = 10;
         block-spacing = 1;
 
         fade-out = 0.5;
 
-        background = colorscheme.base.hex;
+        background = colorscheme.crust.hex;
         border-color = colorscheme.secondary.hex;
         bar-fg-color = colorscheme.secondary.hex;
         bar-bg-color = colorscheme.surface0.hex;
