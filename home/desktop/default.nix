c35e067175adfee1950e493ba97f85858e5e4359
Zakhar Voloshchuk <3elduu@gmail.com>
2024-06-16 17:59:57 +0300

    feat(home): add games directory

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -11,5 +11,6 @@
     ./text.nix
     ./code.nix
     ./xremap.nix
+    ./games.nix
   ];
 }
---
+++ home/desktop/games.nix
@@ -0,0 +1,6 @@
+{ pkgs, ... }: {
+  home.packages = [
+    # Minecraft launcher
+    pkgs.prismlauncher
+  ];
+}
