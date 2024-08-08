9f4c8ee8cc0bd39e6a777b17f6c5b3f62fe54b34
Zakhar Voloshchuk <3elduu@gmail.com>
2024-08-08 17:05:51 +0300

    feat(sway): hide cursor after 1 second of no mouse movement

--- home/wm/sway.nix
+++ home/wm/sway.nix
@@ -115,6 +115,11 @@ in
           xkb_options = "grp:caps_toggle";
         };
       };
+      seat = {
+        "*" = {
+          hide_cursor = "1000"; # Hide cursor after 1 second
+        };
+      };
       output = {
         "*" = {
           bg = lib.mkForce "${config.lib.stylix.colors.withHashtag.base02} solid_color";
