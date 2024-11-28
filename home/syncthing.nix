9fd6638b3cfb01be562063813feb7c1642f5f516
Zakhar Voloshchuk <3elduu@gmail.com>
2024-11-28 16:52:05 +0100

    configuring folders declaratively does not work. whatever

--- home/syncthing.nix
+++ home/syncthing.nix
@@ -8,31 +8,5 @@
 
   services.syncthing = {
     enable = true;
-
-    settings = {
-      devices.syncthing-server = {
-        name = "syncthing-server";
-        id = "72LT3M6-I2QKAJL-HZV65YI-7KI2URR-GT2IXJB-R7JCGJU-RRJULVB-VPMWAQL";
-        autoAcceptFolders = true;
-      };
-      folders = {
-        dev = {
-          id = "bdwea-6f56n";
-          path = "~/dev";
-        };
-        downloads = {
-          id = "jm5ry-9lyhz";
-          path = "~/dl";
-        };
-        doc = {
-          id = "siwwk-3zedz";
-          path = "~/doc";
-        };
-        pic = {
-          id = "onada-w2j7c";
-          path = "~/pic";
-        };
-      };
-    };
   };
 }
